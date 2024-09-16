8c9fcff7b3e09aea94d8a537e260f7a3f96d5c75
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-09-17 00:41:58 +0300

    Add project files.

---
+++ Tuyiu.KosyanovDV.Sprint1.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tuyiu.KosyanovDV.Sprint1.Task0.V4.Lib;
+
+namespace Tuyiu.KosyanovDV.Sprint1.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void validExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}

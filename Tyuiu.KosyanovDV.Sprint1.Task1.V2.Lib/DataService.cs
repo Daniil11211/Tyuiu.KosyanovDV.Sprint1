a6909430551248c75fc4b58330caada16a202a01
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-09-17 12:21:52 +0300

    Task1

---
+++ Tyuiu.KosyanovDV.Sprint1.Task1.V2.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KosyanovDV.Sprint1.Task1.V2.Lib
+{
+
+    public class DataService : ISprint1Task1V2
+    {
+        public double Calculate(double x, double y)
+        {
+            return x * y / (5 + x);
+        }
+    }
+}
---
+++ Tyuiu.KosyanovDV.Sprint1.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KosyanovDV.Sprint1.Task1.V2.Lib;
+
+namespace Tyuiu.KosyanovDV.Sprint1.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual (0.5, res);
+
+        }
+    }
+}

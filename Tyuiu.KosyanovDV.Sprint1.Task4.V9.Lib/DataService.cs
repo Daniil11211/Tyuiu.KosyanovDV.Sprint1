e95db2bb60cc658a97ee1851a41e2abd23c300dc
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-09-19 22:57:51 +0300

    Task4

--- Tyuiu.KosyanovDV.Sprint1.Task4.V9.Lib/DataService.cs
+++ Tyuiu.KosyanovDV.Sprint1.Task4.V9.Lib/DataService.cs
@@ -11,7 +11,8 @@ namespace Tyuiu.KosyanovDV.Sprint1.Task4.V9.Lib
     {
         public double Calculate(double x, double y)
         {
-            return double.Round(((Math.Log(x * y)) / (x - Math.Sqrt(1 + Math.Pow(y, 2)))), 3);
+            double res = Math.Log(x * y) / x - Math.Sqrt(1 + Math.Pow(y, 2));
+            return double.Round(res, 3);
         }
     }
 }

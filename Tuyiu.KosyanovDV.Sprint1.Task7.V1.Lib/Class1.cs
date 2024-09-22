f13bb19a5b2e89ddc3297472f07e08d55b1dcbe3
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-09-22 22:37:14 +0300

    Task7

--- Tuyiu.KosyanovDV.Sprint1.Task7.V1.Lib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tuyiu.KosyanovDV.Sprint1.Task7.V1.Lib
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Tuyiu.KosyanovDV.Sprint1.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tuyiu.KosyanovDV.Sprint1.Task7.V1.Lib
+{
+    public class DataService : ISprint1Task7V1
+    {
+        public double Calculate(double a, double b, double c)
+        {
+            return double.Round((((b + Math.Sqrt(Math.Pow(b, 2) + 4 * a * c)) / (2 * a)) - Math.Pow(a, 3) * c + Math.Pow(b, -2)), 3);
+        }
+    }
+}
+
